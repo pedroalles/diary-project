6bca138772a64782778c68fd5276bfa5c08652ab
pedroalles <pedroalles@gmail.com>
2021-09-25 16:49:45 -0300

    Add taskform

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 
+import TaskForm from './components/TaskForm';
+
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={ store }>
-      Solution 2 here
+      <TaskForm />
     </Provider>
   );
 }
