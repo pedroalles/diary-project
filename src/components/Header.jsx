7cccde001ad2d5ce01a45b6267a4123b12d19a9f
pedroalles <pedroalles@gmail.com>
2021-09-28 19:23:43 -0300

    Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 import { TaskForm, SearchBar } from '.';
 
 const StyledHeader = styled.header`
-  /* background-color: gray; */
   display: flex;
   justify-content: space-between;
   margin: 6px auto;
